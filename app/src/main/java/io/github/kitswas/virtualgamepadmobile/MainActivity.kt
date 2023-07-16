0493f70eeff442105e6c7444f7cb949343582e25
kitswas <90329875+kitswas@users.noreply.github.com>
2023-07-16 15:09:17 +0530

    Added main menu

--- app/src/main/java/io/github/kitswas/virtualgamepadmobile/MainActivity.kt
+++ app/src/main/java/io/github/kitswas/virtualgamepadmobile/MainActivity.kt
@@ -3,14 +3,20 @@ package io.github.kitswas.virtualgamepadmobile
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import io.github.kitswas.virtualgamepadmobile.ui.theme.VirtualGamePadMobileTheme
+import kotlin.system.exitProcess
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +25,7 @@ class MainActivity : ComponentActivity() {
             VirtualGamePadMobileTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    MainMenu()
                 }
             }
         }
@@ -27,14 +33,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainMenu() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        var name by remember { mutableStateOf("Start") }
+        Button(onClick = { name = "Clicked..." }, shape = CircleShape) {
+            Text(text = name)
+        }
+        Button(onClick = { }, shape = CircleShape) {
+            Text(text = "Settings")
+        }
+        Button(onClick = { exitProcess(0) }, shape = CircleShape) {
+            Text(text = "Exit")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     VirtualGamePadMobileTheme {
-        Greeting("Android")
+        MainMenu()
     }
 }
