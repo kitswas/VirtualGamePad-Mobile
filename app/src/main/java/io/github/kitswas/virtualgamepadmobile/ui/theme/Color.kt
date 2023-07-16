2731f93d7bd938b734c17f63e2839833e5cb98ca
kitswas <90329875+kitswas@users.noreply.github.com>
2023-07-16 22:16:54 +0530

    Nice Colours

--- app/src/main/java/io/github/kitswas/virtualgamepadmobile/ui/theme/Color.kt
+++ app/src/main/java/io/github/kitswas/virtualgamepadmobile/ui/theme/Color.kt
@@ -2,7 +2,19 @@ package io.github.kitswas.virtualgamepadmobile.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+
+// Should be the same as the values defined in values/colors.xml
+
+// The colours should be changed to match the colours of the gamepad
+// Neon in the dark for the gamers' eyes
+
+val NeonGreen = Color(0xFF39FF14)
+val NeonBlue = Color(0xFF00FFFF)
+val NeonRed = Color(0xFFFF0000)
+val GlossyGreen = Color(0xFF00AA00)
+val GlossyBlue = Color(0xFF035CC2)
+val GlossyRed = Color(0xFFDD0000)
+val Gold = Color(0xFFFFD700)
+val Silver = Color(0xFFC0C0C0)
+val PureBlack = Color(0xFF000000)
+val PristineWhite = Color(0xFFFFFFFF)
--- app/src/main/java/io/github/kitswas/virtualgamepadmobile/ui/theme/Theme.kt
+++ app/src/main/java/io/github/kitswas/virtualgamepadmobile/ui/theme/Theme.kt
@@ -7,15 +7,15 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = NeonBlue,
+    primaryVariant = NeonGreen,
+    secondary = NeonRed
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = GlossyBlue,
+    primaryVariant = GlossyGreen,
+    secondary = GlossyRed,
 
     /* Other default colors to override
     background = Color.White,
