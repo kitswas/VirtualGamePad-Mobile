38c0b91b1155680b54331dcac5662bf7a9e8c069
kitswas <90329875+kitswas@users.noreply.github.com>
2023-07-17 09:48:23 +0530

    Cool fonts
    Freely usable fonts downloaded from the internet.

--- app/src/main/java/io/github/kitswas/virtualgamepadmobile/ui/theme/Type.kt
+++ app/src/main/java/io/github/kitswas/virtualgamepadmobile/ui/theme/Type.kt
@@ -5,20 +5,22 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import io.github.kitswas.virtualgamepadmobile.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    defaultFontFamily = FontFamily(androidx.compose.ui.text.font.Font(R.font.moonhouse)),
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
     ),
+    button = TextStyle(
+        fontFamily = FontFamily(androidx.compose.ui.text.font.Font(R.font.freedom)),
+        fontWeight = FontWeight.Normal,
+        fontSize = 20.sp
+    ),
+    /* Other default text styles to override
     caption = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
