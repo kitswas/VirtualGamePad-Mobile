f4276a9d4a51f6cdb44a867687ad5689e485f6c1
kitswas <90329875+kitswas@users.noreply.github.com>
2024-06-21 03:06:32 +0530

    Implemented settings using sharedPreferences
    Available setting: colour scheme /dark mode.

--- app/src/main/java/io/github/kitswas/virtualgamepadmobile/ui/screens/SettingsScreen.kt
+++ app/src/main/java/io/github/kitswas/virtualgamepadmobile/ui/screens/SettingsScreen.kt
@@ -1,11 +1,73 @@
 package io.github.kitswas.virtualgamepadmobile.ui.screens
 
+import android.util.Log
+import androidx.preference.PreferenceManager
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.*
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import io.github.kitswas.virtualgamepadmobile.ui.theme.ColorSchemePicker
+import io.github.kitswas.virtualgamepadmobile.ui.theme.VirtualGamePadMobileTheme
+
 @Composable
 fun SettingsScreen(
     navController: NavController = rememberNavController()
 ) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceAround,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Settings Screen")
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(LocalContext.current)
+        val prefEditor = remember {
+            sharedPreferences.edit()
+        }
+
+        LazyVerticalGrid(
+            columns = GridCells.Adaptive(minSize = 160.dp), contentPadding = PaddingValues(16.dp)
+        ) {
+            // Add dropdown for color scheme
+            item {
+                ColorSchemePicker { colorScheme ->
+                    prefEditor.putInt("color_scheme", colorScheme.ordinal)
+                }
+            }
+
+        }
+
+        Row(
+            modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Button(onClick = {
+                sharedPreferences.edit().clear().apply()
+                // Recompose the settings screen
+
+            }) {
+                Text("Reset")
+            }
+
+            Button(onClick = {
+                val success = prefEditor.commit()
+                Log.i("SettingsScreen", "Saved settings: $success")
+                navController.popBackStack()
+            }) {
+                Text("Save")
+            }
+
+            Button(onClick = {
+                navController.popBackStack()
+            }) {
+                Text("Back")
+            }
+        }
+    }
 }
 
 
--- app/src/main/java/io/github/kitswas/virtualgamepadmobile/ui/theme/Theme.kt
+++ app/src/main/java/io/github/kitswas/virtualgamepadmobile/ui/theme/Theme.kt
@@ -1,10 +1,17 @@
 package io.github.kitswas.virtualgamepadmobile.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.preference.PreferenceManager
+import io.github.kitswas.virtualgamepadmobile.ui.composables.ListItemPicker
 
 private val DarkColorPalette = darkColorScheme(
     primary = NeonBlue,
@@ -36,10 +43,48 @@ enum class ColorScheme {
 }
 
 @Composable
+fun ColorSchemePicker(
+    modifier: Modifier = Modifier, onColorSchemeSelected: (ColorScheme) -> Unit = { _ -> }
+) {
+
+    val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(LocalContext.current)
+
+    ListItemPicker(
+        modifier = modifier,
+        list = ColorScheme.values().asIterable(),
+        default = ColorScheme.fromInt(
+            sharedPreferences.getInt(
+                "color_scheme",
+                ColorScheme.SYSTEM.ordinal
+            )
+        ),
+        label = "Color Scheme",
+        onItemSelected = onColorSchemeSelected
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ColorSchemePickerPreview() {
+    VirtualGamePadMobileTheme {
+        ColorSchemePicker(Modifier.padding(16.dp))
+    }
+}
+
+@Composable
 fun VirtualGamePadMobileTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
+    val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(LocalContext.current)
+
+    val darkMode: ColorScheme =
+        ColorScheme.fromInt(sharedPreferences.getInt("color_scheme", ColorScheme.SYSTEM.ordinal))
+    val darkTheme: Boolean = when (darkMode) {
+        ColorScheme.LIGHT -> false
+        ColorScheme.DARK -> true
+        ColorScheme.SYSTEM -> isSystemInDarkTheme()
+    }
+
     val colorScheme = if (darkTheme) {
         DarkColorPalette
     } else {
