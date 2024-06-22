e413043ff408ff21b087b447ff9d18a889375a29
kitswas <90329875+kitswas@users.noreply.github.com>
2024-06-22 15:51:44 +0530

    Typography changes

--- app/src/main/java/io/github/kitswas/virtualgamepadmobile/ui/theme/Type.kt
+++ app/src/main/java/io/github/kitswas/virtualgamepadmobile/ui/theme/Type.kt
@@ -10,17 +10,17 @@ import io.github.kitswas.virtualgamepadmobile.R
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily(androidx.compose.ui.text.font.Font(R.font.android_insomnia)),
+        fontFamily = FontFamily(androidx.compose.ui.text.font.Font(R.font.gamefly)),
         fontWeight = FontWeight.Normal,
         fontSize = 20.sp
     ),
     bodyMedium = TextStyle(
-        fontFamily = FontFamily(androidx.compose.ui.text.font.Font(R.font.android_insomnia)),
+        fontFamily = FontFamily(androidx.compose.ui.text.font.Font(R.font.gamefly)),
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     ),
     bodySmall = TextStyle(
-        fontFamily = FontFamily(androidx.compose.ui.text.font.Font(R.font.android_insomnia)),
+        fontFamily = FontFamily(androidx.compose.ui.text.font.Font(R.font.gamefly)),
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp
     ),
@@ -38,5 +38,20 @@ val Typography = Typography(
         fontFamily = FontFamily(androidx.compose.ui.text.font.Font(R.font.astro_space)),
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = FontFamily(androidx.compose.ui.text.font.Font(R.font.android_insomnia)),
+        fontWeight = FontWeight.Normal,
+        fontSize = 32.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = FontFamily(androidx.compose.ui.text.font.Font(R.font.android_insomnia)),
+        fontWeight = FontWeight.Normal,
+        fontSize = 28.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = FontFamily(androidx.compose.ui.text.font.Font(R.font.android_insomnia)),
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp
     ),
 )
