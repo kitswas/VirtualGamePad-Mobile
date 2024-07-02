3a33617ff16b8d56bdf1ebbc473649b4b2671523
kitswas <90329875+kitswas@users.noreply.github.com>
2024-07-02 22:39:31 +0530

    Extracted ConnectionViewModel to separate file

--- app/src/main/java/io/github/kitswas/virtualgamepadmobile/network/ConnectionState.kt
+++ app/src/main/java/io/github/kitswas/virtualgamepadmobile/network/ConnectionState.kt
@@ -1,13 +1,7 @@
 package io.github.kitswas.virtualgamepadmobile.network
 
-import android.util.Log
-import androidx.lifecycle.ViewModel
-import io.github.kitswas.VGP_Data_Exchange.GamepadReading
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
 import java.net.Socket
+
 
 data class ConnectionState(
     var connected: Boolean = false,
@@ -15,83 +9,3 @@ data class ConnectionState(
     val port: Int = -1,
     var socket: Socket? = null
 )
-
-class ConnectionViewModel : ViewModel() {
-
-    private val tag = "ConnectionViewModel"
-
-    // Expose screen UI state
-    private val _uiState = MutableStateFlow(ConnectionState())
-    val uiState: StateFlow<ConnectionState> = _uiState.asStateFlow()
-
-    // Handle business logic
-    /**
-     * Connect to a server at the given IP address and port.
-     * This is a blocking call.
-     */
-    fun connect(ipAddress: String, port: Int) {
-        try {
-            val socket = Socket()
-            // Disable Nagle's algorithm
-            socket.tcpNoDelay = true
-            // low latency > short connection time > high bandwidth
-            socket.setPerformancePreferences(1, 2, 0)
-            // This is a generous timeout to establish a connection
-            // Typically the ping should be less than 50ms for gaming purposes
-            val timeout = 0 // in milliseconds, 0 means infinite
-            socket.connect(java.net.InetSocketAddress(ipAddress, port), timeout)
-            Log.d(tag, socket.toString())
-
-            _uiState.update { currentState ->
-                currentState.copy(
-                    connected = true,
-                    ipAddress = ipAddress,
-                    port = port,
-                    socket = socket
-                )
-            }
-        } catch (e: Exception) {
-            Log.e(tag, e.toString())
-        }
-    }
-
-    /**
-     * This is primarily for testing purposes.
-     */
-    @Suppress("unused")
-    fun sendString(string: String) {
-        try {
-            if (_uiState.value.connected && _uiState.value.socket != null) {
-                _uiState.value.socket!!.outputStream.write(string.toByteArray())
-            }
-        } catch (e: Exception) {
-            Log.e(tag, e.toString())
-        }
-    }
-
-    fun sendGamepadState(gamepadState: GamepadReading) {
-        try {
-            if (_uiState.value.connected && _uiState.value.socket != null) {
-                gamepadState.marshal(_uiState.value.socket!!.outputStream, null)
-            }
-        } catch (e: Exception) {
-            Log.e(tag, e.toString())
-        }
-    }
-
-    fun disconnect() {
-        try {
-            _uiState.value.socket?.close()
-            _uiState.update { currentState ->
-                currentState.copy(
-                    connected = false,
-                    ipAddress = "",
-                    port = -1,
-                    socket = null
-                )
-            }
-        } catch (e: Exception) {
-            Log.e(tag, e.toString())
-        }
-    }
-}
