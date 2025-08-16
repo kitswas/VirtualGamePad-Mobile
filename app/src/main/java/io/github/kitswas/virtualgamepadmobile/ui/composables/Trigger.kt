2787757c96c38f0301c3b18df4b2f4760b45cab7
kitswas <90329875+kitswas@users.noreply.github.com>
2025-08-16 15:02:42 +0530

    Improved Trigger shape

--- app/src/main/java/io/github/kitswas/virtualgamepadmobile/ui/composables/Trigger.kt
+++ app/src/main/java/io/github/kitswas/virtualgamepadmobile/ui/composables/Trigger.kt
@@ -5,8 +5,11 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.AbsoluteCutCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -17,10 +20,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import io.github.kitswas.VGP_Data_Exchange.GamepadReading
-import io.github.kitswas.virtualgamepadmobile.ui.theme.Shapes
 import io.github.kitswas.virtualgamepadmobile.ui.utils.HapticUtils
 
 enum class TriggerType {
@@ -62,20 +65,48 @@ fun Trigger(
         TriggerType.RIGHT -> "RT"
     }
 
+    val triggerShape = when (type) {
+        TriggerType.LEFT -> AbsoluteCutCornerShape(
+            topRight = size * 0.4f,
+        )
+
+        TriggerType.RIGHT -> AbsoluteCutCornerShape(
+            topLeft = size * 0.4f,
+        )
+    }
+
     Box(
         modifier = modifier
-            .size(size)
+            .width(size)
+            .height(size * 1.25f)
             .fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
         Button(
             onClick = {},
             modifier = Modifier.fillMaxSize(),
-            shape = Shapes.small,
+            shape = triggerShape,
             border = BorderStroke(2.dp, MaterialTheme.colorScheme.outline),
             interactionSource = interactionSource,
         ) {
             Text(text)
         }
+    }
+}
+
+@Preview
+@Composable
+fun TriggerPreview() {
+    Row {
+        Trigger(
+            type = TriggerType.LEFT,
+            size = 64.dp,
+            gamepadState = GamepadReading()
+        )
+        Trigger(
+            type = TriggerType.RIGHT,
+            size = 64.dp,
+            gamepadState = GamepadReading()
+        )
     }
 }
